2f0b1752613d97da1da9e550beb767ea7824db57
Raep9717 <42880367+Raep9717@users.noreply.github.com>
2018-11-24 23:05:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/root/testlistapais/Pais.java
@@ -0,0 +1,40 @@
+package com.example.root.testlistapais;
+
+import android.graphics.drawable.Drawable;
+
+public class Pais {
+
+    private int icono;
+    private String nombre;
+    private String dominio;
+
+    public Pais(int icono, String nombre, String dominio) {
+        this.icono = icono;
+        this.nombre = nombre;
+        this.dominio = dominio;
+    }
+
+    public int getIcono() {
+        return icono;
+    }
+
+    public void setIcono(int icono) {
+        this.icono = icono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDominio() {
+        return dominio;
+    }
+
+    public void setDominio(String dominio) {
+        this.dominio = dominio;
+    }
+}
